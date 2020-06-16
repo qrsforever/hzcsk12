f1a61d14178633138390d23a8ccb4e78d9c81c28
lidongai <lidongai@hzcsai.com>
2020-06-16 22:24:20 +0800

    little mod

--- scripts/deps_install.sh
+++ scripts/deps_install.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
+check=`test -e /etc/exports && echo 1 || echo 0`
+if [[ x$check == x0 ]]
+then
+    sudo apt install -y nfs-common nfs-kernel-server
+fi
+
 check=`python3 -c "import ssl" 2>&1`
 pyver=`python3 --version | cut -d\  -f2`
 if [[ x$check != x ]] || [[ $pyver != "3.6.8" ]]
 then
+    sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
     echo "############"
-    echo "1. sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev"
-    echo "2. build the python3.6.8: ./configure --enable-shared --enable-optimizations;  make; sudo make install"
+    echo "build the python3.6.8: ./configure --enable-shared --enable-optimizations;  make; sudo make install"
+    echo "sudo echo "/usr/local/lib" > python3.6.config; sudo /sbin/ldconfig -v"
     echo "more see: https://tecadmin.net/install-python-3-6-ubuntu-linuxmint/"
     echo "############"
     exit -1
 else
     echo "install python3 with ssl ok!"
-fi
-
-check=`test -e /etc/exports && echo 1 || echo 0`
-if [[ x$check == x0 ]]
-then
-    sudo apt install -y nfs-common nfs-kernel-server
 fi
 
 check=`python3 -c "import flask" 2>&1`
@@ -57,6 +58,25 @@ check=`python3 -c "import docker" 2>&1`
 if [[ x$check != x ]]
 then
     sudo pip3 install docker
+    sudo apt install -f nvidia-docker2
+    sudo gpasswd -a $USER docker; newgrp docker
+    sudo systemctl restart docker.service
+    echo "##############"
+    echo "append in /etc/docker/daemon.json"
+    echo "{"
+    echo "    \"runtimes\": {"
+    echo "       \"nvidia\": {"
+    echo "            \"path\": \"nvidia-container-runtime\","
+    echo "            \"runtimeArgs\": []",
+    echo "        }"
+    echo "    },"
+    echo "    \"default-runtime\": \"nvidia\","
+    echo "    \"registry-mirrors\": [\"https://registry.docker-cn.com\"],"
+    echo "    \"insecure-registries\":[\"10.255.0.58:9500\"]"
+    echo "}"
+    echo ""
+    echo "sudo systemctl restart docker.service"
+    echo "##############"
 else
     echo "py docker ok!"
 fi
