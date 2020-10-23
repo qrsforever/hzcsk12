17906603bf491ec9d304767935d927504f06060e
lidongai <lidongai@hzcsai.com>
2020-10-23 11:31:23 +0800

    save

--- scripts/consul/checks/check_critical.py
+++ scripts/consul/checks/check_critical.py
@@ -1,19 +1,41 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 import json
-import hashlib
-from urllib.parse import quote_plus, unquote_plus
-from urllib.request import urlopen
+# import hashlib
+# from urllib.parse import quote_plus, unquote_plus
+# from urllib.request import urlopen
+
+def debug_out(fname, data):
+    with open('/tmp/%s.txt' % fname, 'a') as fw:
+        fw.write(data)
 
 def main():
-    consul_stdin = json.load(sys.stdin)
-    data = json.dumps(consul_stdin)
-    print(data)
-    with open('/tmp/k12check.txt', 'w+') as fw:
-        fw.write(data)
+    debug_out('k12aaa', 'hello'):
+    try:
+        data = sys.stdin
+        host = os.environ.get('CONSUL_ADDR', None)
+        port = os.environ.get('CONSUL_PORT', None)
+        if host is None or port is None:
+            return
+        debug_out('consul_stdin', data)
+        data = json.load(sys.stdin)
+        if len(data) == 0:
+            return
+
+        content = {}
+        for item in data:
+            node = item["Node"]
+            name = item["ServiceName"]
+            if node not in content:
+                content[node] = []
+            content[node].append(name)
+        debug_out('report_content', json.dumps(content))
+    except Exception as err:
+        debug_out('err', '{}'.format(err))
+        
 
 if __name__ == "__main__":
     main()
-
