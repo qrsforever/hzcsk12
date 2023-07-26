ae772aa50536acbaaff27b3943fb0d3aef10a7d3
Li Dong <erlangai@qq.com>
2023-07-26 18:03:41 +0800

    save

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -3,7 +3,7 @@
 firewall-cmd --add-port=8119/tcp --permanent
 firewall-cmd --add-port=9119/tcp --permanent
 
-firewall-cmd --add-port=8139/tcp --permanent
-firewall-cmd --add-port=8179/tcp --permanent
+# firewall-cmd --add-port=8139/tcp --permanent
+# firewall-cmd --add-port=8179/tcp --permanent
 
 firewall-cmd --reload
